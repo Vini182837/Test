20d3311f58d1f7c479ae3e64ff739b07a1c2dd99
Vini182837 <marcosviniciusbispodejesus26@gmail.com>
2024-11-08 22:04:53 -0300

    Adicionar arquivo de script

---
+++ deb-trix.sh
@@ -0,0 +1,6 @@
+DISTRO_ARCH="aarch64"
+DISTRO_NAME="Debian (Trixie)"
+
+TARBALL_STRIP_OPT=0
+TARBALL_URL['aarch64']="https://github.com/Vini182837/Test/releases/download/Rootfs/my_rootfs.tar.gz"
+TARBALL_SHA256['aarch64']="4a9f7ec2ba8b79152290efab85a611ad49c4e2c4fff9f2ee156899b3ae99788b"
